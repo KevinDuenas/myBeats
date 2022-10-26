7cf93f33815a995beb4a352716a909393f262a51
Kevin Duenas <kevinduenas@hotmail.es>
2022-10-26 11:56:01 -0500

    Initial Commit

---
+++ myBeats/myBeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBeatsApp.swift
+//  myBeats
+//
+//  Created by Kevin Duenas on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct myBeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
